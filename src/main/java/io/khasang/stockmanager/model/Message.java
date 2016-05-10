ed22fa3f2720b03eca9861b08543c8edde8af496
Kuznetsov Sergey <azon.sk@gmail.com>
2016-05-10 21:35:32 +0300

    add basic spring xml structure and beans

---
+++ src/main/java/io/khasang/stockmanager/model/Message.java
@@ -0,0 +1,16 @@
+package io.khasang.stockmanager.model;
+
+public class Message {
+    private String messageInfo;
+
+    public Message() {
+    }
+
+    public String getMessageInfo() {
+        return messageInfo;
+    }
+
+    public void setMessageInfo(String messageInfo) {
+        this.messageInfo = messageInfo;
+    }
+}

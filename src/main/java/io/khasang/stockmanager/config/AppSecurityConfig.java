52d4f1ee85fac4165adafa9be8b1166083bff55b
developermsv <developermsv@yandex.ru>
2016-05-24 19:40:06 +0300

    add database authorization

--- src/main/java/io/khasang/stockmanager/config/AppSecurityConfig.java
+++ src/main/java/io/khasang/stockmanager/config/AppSecurityConfig.java
@@ -2,6 +2,7 @@ package io.khasang.stockmanager.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,10 +12,21 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("user").password("user").roles("USER");
-        auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
-        auth.inMemoryAuthentication().withUser("superadmin").password("superadmin").roles("SUPERADMIN");
+        auth.jdbcAuthentication().dataSource(jdbcTemplate.getDataSource())
+                .usersByUsernameQuery(
+                        "select username,password, enabled from users where username=?")
+                .authoritiesByUsernameQuery(
+                        "select u.username, r.role_name \n" +
+                                "from user_roles ur\n" +
+                                "join users u on u.user_id = ur.user_id and u.username = ?  \n" +
+                                "join role r on r.role_id = ur.role_id");
+//        auth.inMemoryAuthentication().withUser("user").password("user").roles("USER");
+//        auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
+//        auth.inMemoryAuthentication().withUser("superadmin").password("superadmin").roles("SUPERADMIN");
     }
 
     @Override

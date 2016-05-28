44a7b54445a5946b9cee9ee3872e8a41b46a51dd
cliff <r2d2.hell@gmail.com>
2016-05-28 14:19:27 +0300

    move resources to view

--- src/main/java/io/khasang/stockmanager/config/application/WebConfig.java
+++ src/main/java/io/khasang/stockmanager/config/application/WebConfig.java
@@ -24,9 +24,9 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/css/**").addResourceLocations("/WEB-INF/css/");
-        registry.addResourceHandler("/jpg/**").addResourceLocations("/WEB-INF/images/");
+        registry.addResourceHandler("/css/**").addResourceLocations("/WEB-INF/views/css/");
+        registry.addResourceHandler("/jpg/**").addResourceLocations("/WEB-INF/views/images/");
         registry.addResourceHandler("/bootstrap/**").addResourceLocations("/WEB-INF/views/Content/");
-        registry.addResourceHandler("/js/**").addResourceLocations("/WEB-INF/js/");
+        registry.addResourceHandler("/js/**").addResourceLocations("/WEB-INF/views/js/");
     }
 }

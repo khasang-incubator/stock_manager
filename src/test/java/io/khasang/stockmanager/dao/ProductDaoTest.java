0743911e588b464df883f757ce2b545f5c2b30e5
cliff <r2d2.hell@gmail.com>
2016-05-29 16:04:06 +0300

    add ProductDaoTest

--- src/test/java/io/khasang/stockmanager/dao/ProductDaoTest.java
+++ src/test/java/io/khasang/stockmanager/dao/ProductDaoTest.java
@@ -1,22 +1,24 @@
-package io.khasang.stockmanager.entity;
+package io.khasang.stockmanager.dao;
+
 
 import io.khasang.stockmanager.config.application.WebConfig;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import static org.junit.Assert.assertNotNull;
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
 @ContextConfiguration(classes = {WebConfig.class})
-public class ProjectTest {
-    
+public class ProductDaoTest {
+
+    @Autowired
+    ProjectDao projectDao;
+
     @Test
-    public void testProductEntity(){
-        assertNotNull(new Project());
+    public void testInsertToTable(){
+        projectDao.isertToTable(new Project());
+        assertEquals("Result wrong", "sucessfully insert to table", projectDao.getResult());
     }
 
 }

f41327ac71514b37eb3fd0e15f03b30ce449a514
Inlinebrother <eee127@yandex.ru>
2016-05-29 15:51:53 +0300

    add ProductTest

---
+++ src/test/java/io/khasang/stockmanager/entity/ProjectTest.java
@@ -0,0 +1,22 @@
+package io.khasang.stockmanager.entity;
+
+import io.khasang.stockmanager.config.application.WebConfig;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = {WebConfig.class})
+public class ProjectTest {
+    
+    @Test
+    public void testProductEntity(){
+        assertNotNull(new Project());
+    }
+}

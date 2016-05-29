92ea116c81caf3d315019b6a260764d0b660c8d1
Inlinebrother <eee127@yandex.ru>
2016-05-29 17:00:50 +0300

    add Project fail test realization

--- src/main/java/io/khasang/stockmanager/dao/ProjectDao.java
+++ src/main/java/io/khasang/stockmanager/dao/ProjectDao.java
@@ -1,19 +1,55 @@
 package io.khasang.stockmanager.dao;
 
+import io.khasang.stockmanager.entity.Offer;
 import io.khasang.stockmanager.entity.Project;
+import org.hibernate.HibernateException;
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Component
 public class ProjectDao {
-
+    @Autowired
+    SessionFactory sessionFactory;
     private String result;
     public void insertToTable(Project project){
 
+        Session session = sessionFactory.openSession();
 
+//            session.beginTransaction();
+//            String sql = "SELECT * FROM Project";
+//            SQLQuery query = session.createSQLQuery(sql);
+//            query.addEntity(Project.class);
 
+        try {
+            session.beginTransaction();
+
+            //Receive status with specify id - required for check field state = at status end or not.
+            List result = session.createCriteria(Project.class, "id")
+                    .list();
+
+            session.save(project);
+            session.getTransaction().commit();
+
+        } catch (HibernateException e) {
+            session.getTransaction().rollback();
+          //  resultQuery = "fail: " + e;
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
     }
+
 
     public String getResult(){
         return "sucessfully insert to table";
     }
 }
+
--- src/main/java/io/khasang/stockmanager/entity/Project.java
+++ src/main/java/io/khasang/stockmanager/entity/Project.java
@@ -1,6 +1,29 @@
 package io.khasang.stockmanager.entity;
 
+import javax.persistence.*;
+
+@Entity
 public class Project {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    //@Column(nullable = false)
+    private String projectName;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
 }

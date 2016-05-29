d274d21b70f6e42269362e5085e2bd708b3f3074
truesik <truesik@gmail.com>
2016-05-29 16:36:06 +0300

    created new dao test class

---
+++ src/test/java/io/khasang/stockmanager/dao/ProjectDAOTest.java
@@ -0,0 +1,30 @@
+package io.khasang.stockmanager.dao;
+
+import io.khasang.stockmanager.entity.Project;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ProjectDAOTest {
+    @Autowired
+    private Project project;
+
+    @Test
+    public void addTest() {
+
+    }
+
+    @Test
+    public void deleteTest() {
+
+    }
+
+    @Test
+    public void getAll() {
+
+    }
+
+    @Test
+    public void getById() {
+
+    }
+}

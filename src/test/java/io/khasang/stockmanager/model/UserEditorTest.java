ddd3cb605d485f1d441266c65a78201844884779
Yukon85 <yurev.ks@gmail.com>
2016-06-04 17:54:56 +0800

    add new tests for UserEditors

--- src/test/java/io/khasang/stockmanager/model/UserEditorTest.java
+++ src/test/java/io/khasang/stockmanager/model/UserEditorTest.java
@@ -1,45 +1,66 @@
 package io.khasang.stockmanager.model;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.security.InvalidParameterException;
 
-//@RunWith(SpringJUnit4ClassRunner.class)
 public class UserEditorTest {
-    String id = null;
-    String name = null;
-    String surname = null;
-    String login = null;
-    String password = null;
-    String email = null;
-    String role = null;
-    String newUser = null;
+    String id;
+    String name;
+    String surname;
+    String login;
+    String password;
+    String email;
+    String role;
+    String newUser;
 
     UserEditor userEditor = new UserEditor();
 
+    @Before
+    public void setup() {
+        id = "";
+        name = "";
+        surname = "";
+        login = "";
+        password = "";
+        email = "";
+        role = "";
+        newUser = "";
+    }
+
     @Test(expected = InvalidParameterException.class)
     public void allNullsTest() {
-        userEditor.defineUserOperationsByParams(id, name, surname, login, password, email, role, newUser);
-    }
-
-    @Test
-    public void importantParamsTest() {
-        login = "sdfgs";
-        role = "sdfsd";
+        id = null;
+        name = null;
+        surname = null;
+        login = null;
+        password = null;
+        email = null;
+        role = null;
+        newUser = null;
         userEditor.defineUserOperationsByParams(id, name, surname, login, password, email, role, newUser);
     }
 
     @Test(expected = InvalidParameterException.class)
-    public void defineUserOperationsByParamsTest() {
+    public void importantParamsTest() {
+        login = null;
+        role = null;
+        userEditor.defineUserOperationsByParams(id, name, surname, login, password, email, role, newUser);
+    }
+
+    @Test(expected = InvalidParameterException.class)
+    public void passwordForNewUserTest() {
         newUser = "on";
         password = null;
         userEditor.defineUserOperationsByParams(id, name, surname, login, password, email, role, newUser);
     }
 
-//    @Test(expected = InvalidParameterException.class)
+//    @Test
 //    public void defineUserOperationsByParamsTest() {
-//        newUser = "on";
-//        password = null;
-//        userEditor.defineUserOperationsByParams(id, name, surname, login, password, email, role, newUser);
+//        newUser = "off";
+//        password = ;
+//        Assert.assertEquals("Something wrong", userEditor.defineUserOperationsByParams(id, name, surname,
+//                login, password, email, role, newUser));
 //    }
 }

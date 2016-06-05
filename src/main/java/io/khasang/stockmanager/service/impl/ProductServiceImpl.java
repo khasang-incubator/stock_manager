c7c748f87a27f9f5d1ae422257f3a8e7ac66f4f1
Жмылев Алексей <bezblagodatnpst@gmail.com>
2016-06-05 16:08:10 +0300

    fix delete method

--- src/main/java/io/khasang/stockmanager/service/impl/ProductServiceImpl.java
+++ src/main/java/io/khasang/stockmanager/service/impl/ProductServiceImpl.java
@@ -31,7 +31,7 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public void delete(int id) {
         Product product = new Product();
-        product.setId(1);
+        product.setId(id);
         delete(product);
     }
 

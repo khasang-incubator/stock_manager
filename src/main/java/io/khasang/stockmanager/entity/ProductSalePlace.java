d077d5f182f4470d4b548f3004d7ef9553be8063
developermsv <developermsv@yandex.ru>
2016-06-27 15:57:17 +0300

    add new entity ProductSalePlace

---
+++ src/main/java/io/khasang/stockmanager/entity/ProductSalePlace.java
@@ -0,0 +1,73 @@
+package io.khasang.stockmanager.entity;
+
+import org.hibernate.validator.constraints.NotEmpty;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "product_sale_places")
+public class ProductSalePlace {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    @NotEmpty
+    @Column(name = "product_id")
+    private Long productId;
+
+    @NotNull
+    @NotEmpty
+    @Column(nullable = false)
+    private float x;
+
+    @NotNull
+    @NotEmpty
+    @Column(nullable = false)
+    private float y;
+
+    private String description;
+
+    public ProductSalePlace() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}

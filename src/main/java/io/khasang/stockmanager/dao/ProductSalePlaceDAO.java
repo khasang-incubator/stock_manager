e87b11e4f2b4545f0c1d550eaa4c025859428b95
developermsv <developermsv@yandex.ru>
2016-06-28 08:00:14 +0300

    add interface dao

--- src/main/java/io/khasang/stockmanager/dao/ProductSalePlaceDAO.java
+++ src/main/java/io/khasang/stockmanager/dao/ProductSalePlaceDAO.java
@@ -1,4 +1,16 @@
 package io.khasang.stockmanager.dao;
 
+import io.khasang.stockmanager.entity.ProductSalePlace;
+import java.util.List;
+
 public interface ProductSalePlaceDAO {
+    List<ProductSalePlace> getAll();
+
+    List<ProductSalePlace> getProductSalePlacesByProductId(Long id);
+
+    void delete(ProductSalePlace productSalePlace);
+
+    void add(ProductSalePlace productSalePlace);
+
+    void saveOrUpdate(ProductSalePlace productSalePlace);
 }

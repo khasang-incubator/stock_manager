3638180c3c78bf82b470fb166223f56777983428
developermsv <developermsv@yandex.ru>
2016-06-30 19:42:19 +0300

    rename file

---
+++ src/main/java/io/khasang/stockmanager/controller/ProductSalePlaceController.java
@@ -0,0 +1,42 @@
+package io.khasang.stockmanager.controller;
+
+import io.khasang.stockmanager.entity.ProductSalePlace;
+import io.khasang.stockmanager.service.ProductSalePlaceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ProductSalePlaceController {
+    private  Long productId;
+    @Autowired
+    ProductSalePlaceService productSalePlaceService;
+
+    @RequestMapping("/productsaleplace/{productId}")
+    public String productSalePlace(@PathVariable("productId") Long productId, Model model) {
+        model.addAttribute("productSalePlaceList", productSalePlaceService.getAllByProductId(productId));
+        model.addAttribute("productSalePlace", new ProductSalePlace());
+        this.productId = productId;
+        return "productsaleplaces";
+    }
+    @RequestMapping(value = "/productsaleplace/add", method = RequestMethod.POST)
+    public String addProduct(@ModelAttribute("productSalePlace") ProductSalePlace productSalePlace,
+                             BindingResult result) {
+        productSalePlace.setProductId(productId);
+        productSalePlaceService.saveProductSalePlace(productSalePlace);
+        return "redirect:/productsaleplace/"+ productId;
+    }
+
+    @RequestMapping("/productsaleplace/delete/{id}")
+    public String deleteProduct(@PathVariable("id") Long id) {
+        productSalePlaceService.delete(id);
+        return "redirect:/productsaleplace/"+ productId;
+    }
+
+
+}

bb8c093f9e1679e8c7aad969c2fd2a7fbef17f3a
Жмылев Алексей <bezblagodatnpst@gmail.com>
2016-07-14 03:30:45 +0300

    add dbunit for products dao tests

--- src/test/java/io/khasang/stockmanager/model/ProductTest.java
+++ src/test/java/io/khasang/stockmanager/model/ProductTest.java
@@ -1,47 +1,49 @@
 package io.khasang.stockmanager.model;
 
+import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.ExpectedDatabase;
+import com.github.springtestdbunit.assertion.DatabaseAssertionMode;
 import io.khasang.stockmanager.config.application.WebConfig;
 import io.khasang.stockmanager.dao.ProductDAO;
 import io.khasang.stockmanager.entity.Product;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
 @ContextConfiguration(classes = {WebConfig.class})
+@TestExecutionListeners({DependencyInjectionTestExecutionListener.class,
+        TransactionDbUnitTestExecutionListener.class})
+@DatabaseSetup("/productTestData.xml")
 @Transactional
 public class ProductTest {
     @Autowired
     ProductDAO productDAO;
 
-    @Before
-    public void before() {
-        productDAO.deleteAll();
-        Product product = new Product();
-        product.setName("Test");
-        product.setDescription("Description");
-        product.setUserId(1L);
-        productDAO.save(product);
-    }
-
     @Test
+    @ExpectedDatabase(value = "/productTest-save.xml", assertionMode = DatabaseAssertionMode.NON_STRICT)
     public void saveTest() {
         Product product = new Product();
+        product.setId(1L);
         product.setName("Test");
-        product.setDescription("Description");
+        product.setDescription("Description_saved");
         product.setUserId(1L);
         productDAO.save(product);
         Assert.assertNotNull(product.getId());
+        productDAO.getAll();
     }
 
     @Test
+    @ExpectedDatabase(value = "/productTest-delete.xml", assertionMode = DatabaseAssertionMode.NON_STRICT)
     public void deleteTest() {
         productDAO.delete(productDAO.getAll().get(0));
         Assert.assertEquals(0, productDAO.getAll().size());
@@ -51,5 +53,10 @@ public class ProductTest {
     public void getByIdTest() {
         Product product = productDAO.getById(productDAO.getAll().get(0).getId());
         Assert.assertEquals("Test", product.getName());
+    }
+
+    @Test
+    public void getAll() {
+        Assert.assertEquals(1, productDAO.getAll().size());
     }
 }
